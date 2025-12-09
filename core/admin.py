fc881c701e4e5ac600eef6f95212b7710812ff8b
diegoSalasLopez <dead57gamer@gmail.com>
2025-12-09 17:32:57 -0300

    Fix: FINAL User Admin branch field

--- core/admin.py
+++ core/admin.py
@@ -1,23 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Company
+from .models import User, Company, Branch # <-- Importamos todos los modelos necesarios
 
 # Personalizamos el panel de usuario para que muestre tus campos nuevos
 class CustomUserAdmin(UserAdmin):
     # Agregamos una sección nueva en la pantalla de edición
     fieldsets = UserAdmin.fieldsets + (
-        ('Datos TemucoSoft', {'fields': ('rut', 'role', 'company')}),
+        ('Datos TemucoSoft', {'fields': ('rut', 'role', 'company', 'branch')}), # <<< BRANCH AÑADIDO AQUI
     )
     
     # Agregamos los campos también a la pantalla de crear usuario
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Datos TemucoSoft', {'fields': ('rut', 'role', 'company')}),
+        ('Datos TemucoSoft', {'fields': ('rut', 'role', 'company', 'branch')}), # <<< BRANCH AÑADIDO AQUI
     )
     
     # Columnas que se ven en la lista de usuarios
-    list_display = ('username', 'email', 'first_name', 'role', 'company', 'is_staff')
-    list_filter = ('role', 'company', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'role', 'company', 'branch', 'is_staff') # <<< BRANCH AÑADIDO AQUI
+    list_filter = ('role', 'company', 'branch', 'is_staff') # <<< BRANCH AÑADIDO AQUI
 
 # Registramos usando nuestra configuración personalizada
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Company)
